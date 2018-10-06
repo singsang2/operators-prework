62ae50dc700662029449a6c6bf797c0c8ba56b1a
Sung Bae <singsang2@gmail.com>
2018-10-06 03:26:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,17 @@
 require 'pry'
 
 def unsafe?(speed)
-  
+  if speed < 40 || speed > 60
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed<40 || speed > 60) ? true : false
 end
 	
 
